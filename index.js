9fc15ab6af14d23eba185fa2fbe13f6f26dc24e1
Adrian Zambrano <jadrianzc99@gmail.com>
2023-08-15 16:31:11 -0500

    read xlsx and generate report

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+const fs = require('fs');
 const path = require('path');
 const xlsx = require('xlsx');
+const XlsxPopulate = require('xlsx-populate');
 const dotenv = require('dotenv').config();
 
 const pathExcel = path.join(__dirname, '/files/inventario.xlsx');
+const pathExcelPlantilla = path.join(__dirname, '/files/plantilla.xlsx');
+
+const workBook = xlsx.readFile(pathExcel);
+let workSheets = workBook.SheetNames;
+console.log(`Iniciando la generación de reportes de la hoja ${workSheets[0]}`);
+
+const workSheetData = workBook.Sheets[workSheets[0]];
+const fileData = xlsx.utils.sheet_to_json(workSheetData, {
+	header: 1,
+});
+
+const headers = [
+	'ctd_estancias',
+	'cod_estancias',
+	'description',
+	'ctd_equipos',
+	'cod_equipos',
+	'marca',
+	'modelo',
+	'coste_unitario',
+	'num_serie',
+];
+
+fileData.splice(0, 4);
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const convertToJson = async (headers, data) => {
 	const rows = [];
@@ -18,31 +52,56 @@ const convertToJson = async (headers, data) => {
 	return rows;
 };
 
-const workBook = xlsx.readFile(pathExcel);
-let workSheets = workBook.SheetNames;
-console.log(workSheets);
-const workSheetData = workBook.Sheets[workSheets[0]];
-const fileData = xlsx.utils.sheet_to_json(workSheetData, {
-	header: 1,
-	blankRows: false,
-});
-
-const headers = fileData[3];
-fileData.splice(0, 4);
-
 const handleDataReport = (rows, codEstancia, codEquipo) => {
 	const dataFilter = rows.filter(
-		(row) => row['Cod. Estancia ']?.startsWith('CO') && row['Cod. Equipo']?.startsWith('EM')
+		(row) =>
+			row.cod_estancias?.startsWith(codEstancia) && row.cod_equipos?.startsWith(codEquipo)
 	);
 
 	return dataFilter;
 };
 
+const generateReportXlsx = (dataFile) => {
+	// Load an existing workbook
+	for (const data of dataFile) {
+		XlsxPopulate.fromFileAsync(pathExcelPlantilla)
+			.then((workbook) => {
+				const sheet1 = workbook.sheet(0);
+				const cantidadHojas = data?.ctd_equipos;
+
+				for (let index = 1; index <= cantidadHojas - 1; index++) {
+					workbook.cloneSheet(sheet1, `HOJA ${index + 1}`);
+				}
+
+				const cellsValid = [
+					{ cell: 2, value: data?.description },
+					{ cell: 3, value: data?.marca },
+					{ cell: 4, value: data?.modelo },
+					{ cell: 5, value: data?.num_serie || 'S/N' },
+					{ cell: 7, value: '' },
+					{ cell: 9, value: 'CENTRO OBSTÉTRICO' },
+				];
+
+				for (let index = 0; index < cantidadHojas; index++) {
+					for (const cells of cellsValid) {
+						workbook.sheet(index).row(6).cell(cells.cell).value(cells.value);
+					}
+				}
+
+				workbook.activeSheet(0);
+				const nameFile = `archivosGenerados/${data?.description.replace(/\//g, ' ')}.xlsx`;
+
+				workbook.toFileAsync(nameFile);
+			})
+			.catch((error) => console.log(error));
+	}
+
+	console.log(`${dataFile.length} resgistros creados exitosamente.`);
+};
+
 (async () => {
 	const rows = await convertToJson(headers, fileData);
-	console.log(rows);
-
 	const cooEm = handleDataReport(rows, 'CO', 'EM');
-	console.log(cooEm);
-	console.log(cooEm.length);
+
+	generateReportXlsx(cooEm);
 })();
